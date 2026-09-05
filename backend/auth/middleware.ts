734f523f7a79d226b180b543d83b0dba5fe7453d
Reto Brunner <reto@slightlybroken.com>
2026-09-05 08:23:39 +0200

    auth mw and scaffold

---
+++ backend/auth/middleware.ts
@@ -0,0 +1,28 @@
+import { type RequestHandler, type Request } from "express";
+import { type User } from "../users/models.ts";
+
+// note: this isn't remotely pretty, but my TS foo is not good enough to pipe the request var
+// through the RequestHandler type mess... so we forcefully mush it in.
+// at least the damage is scoped to this module...
+
+// Enforces an authenticated user, injecting the user context into req.user
+export function authMw(): RequestHandler {
+	return (req: any, _res, next) => {
+		//TODO: actually implement this
+		const user: User = {
+			id: 1,
+			name: "admin",
+			role: "admin",
+		};
+		req.user = user;
+		next();
+	};
+}
+
+export function userFromRequest(req: Request): User {
+	const user = (req as any).user;
+	if (!user) {
+		throw new Error("no user present, ensure authMw is in the middleware chain before you call this function");
+	}
+	return user;
+}
--- backend/server.ts
+++ backend/server.ts
@@ -1,1 +1,47 @@
-console.log("hello world");
+import express, { type ErrorRequestHandler } from "express";
+import type { AddressInfo } from "node:net";
+import { authMw, userFromRequest } from "./auth/middleware.ts";
+
+const errorHandler: ErrorRequestHandler = (err, _req, res, _next) => {
+	console.error(err.stack);
+	res.sendStatus(500);
+};
+
+const app = express();
+app.disable("x-powered-by");
+app.use(errorHandler);
+
+const apiRouter = express.Router();
+apiRouter.use(authMw());
+
+apiRouter.get("/user", async (req, res) => {
+	const user = userFromRequest(req);
+	res.status(200);
+	res.json(user);
+});
+
+app.use("/api", apiRouter);
+
+const server = app.listen(4444, (err) => {
+	if (err) {
+		console.error(err.message);
+		return;
+	}
+	const addr = server.address();
+	if (addr) {
+		printAddr(addr);
+	}
+});
+
+function printAddr(addr: string | AddressInfo) {
+	let listener: string;
+	if (!addr) {
+		return;
+	} else if (typeof addr === "string") {
+		listener = addr;
+	} else {
+		const host = addr.family === "IPv6" ? `[${addr.address}]` : addr.address;
+		listener = `http://${host}:${addr.port}`;
+	}
+	console.log(`started on ${listener}`);
+}
