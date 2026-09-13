dd595a556e5d1a66db6c9b839a3453befc7ba738
Reto Brunner <reto@slightlybroken.com>
2026-09-13 18:11:13 +0200

    mustString tests

--- backend/helpers/conversions.spec.ts
+++ backend/helpers/conversions.spec.ts
@@ -1,5 +1,5 @@
 import { test, describe, beforeEach, afterEach, expect } from "vitest";
-import { ErrBadInput, safeInt } from "./conversions.ts";
+import { ErrBadInput, mustString, safeInt } from "./conversions.ts";
 
 describe("safeInt", () => {
 	test("good input", () => {
@@ -12,5 +12,16 @@ describe("safeInt", () => {
 		[" 42", "42.0", "42.4", "Nan", "NaN", "inf", "-inf", "42garbage"].map((x) =>
 			expect(() => safeInt(x)).toThrow(ErrBadInput),
 		);
+	});
+});
+
+describe("mustString", () => {
+	test("good input", () => {
+		expect(mustString("34")).toBe("34");
+		expect(mustString("")).toBe("");
+	});
+
+	test("bad input", () => {
+		[null, undefined, 42, -4.7].map((x) => expect(() => mustString(x)).toThrow(ErrBadInput));
 	});
 });
