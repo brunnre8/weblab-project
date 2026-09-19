d2455b13d6b5b61d9477ec3cf3d9eb6a29700206
Reto Brunner <reto@slightlybroken.com>
2026-09-19 13:43:33 +0200

    fe: router input binding for tests
    not sure it's needed, but I'm sure I'll forget otherwise...

--- frontend/src/app/app.spec.ts
+++ frontend/src/app/app.spec.ts
@@ -1,12 +1,12 @@
 import { TestBed } from "@angular/core/testing";
 import { App } from "./app";
-import { provideRouter } from "@angular/router";
+import { provideRouter, withComponentInputBinding } from "@angular/router";
 
 describe("App", () => {
 	beforeEach(async () => {
 		await TestBed.configureTestingModule({
 			imports: [App],
-			providers: [provideRouter([])],
+			providers: [provideRouter([], withComponentInputBinding({ queryParams: false }))],
 		}).compileComponents();
 	});
 
