4eb902309764e9e4bbfa264e010a037b4e211c11
Reto Brunner <reto@slightlybroken.com>
2026-09-19 17:58:04 +0200

    remove dummy route

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,23 +1,16 @@
 import { Routes } from "@angular/router";
 import { Login } from "./login/login";
-import { TodoView } from "./features/todos/dumb/todo-view/todo-view";
 import { TodoGrid } from "./features/todos/todo-grid/todo-grid";
 
 export const routes: Routes = [
-	{
-		path: "",
-		pathMatch: "full",
-		title: "Home",
-		component: TodoView,
-	},
 	{
 		path: "login",
 		title: "login",
 		component: Login,
 	},
 	{
-		path: "list",
-		title: "list",
+		path: "",
+		title: "Home",
 		component: TodoGrid,
 	},
 ];
