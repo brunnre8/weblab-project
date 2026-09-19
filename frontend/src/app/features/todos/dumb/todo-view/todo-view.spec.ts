b717fbf9f37ca7acdaff608a78279adb894e4875
Reto Brunner <reto@slightlybroken.com>
2026-09-19 17:58:27 +0200

    fix test

--- frontend/src/app/features/todos/dumb/todo-view/todo-view.spec.ts
+++ frontend/src/app/features/todos/dumb/todo-view/todo-view.spec.ts
@@ -3,6 +3,15 @@ import { ComponentFixture, TestBed } from "@angular/core/testing";
 import { TodoView } from "./todo-view";
 
 import { provideRouter } from "@angular/router";
+import { Todo } from "../../../../models/todo";
+
+const dummyTodo: Todo = {
+	id: 0,
+	title: "title",
+	body: "body",
+	createdAt: new Date(),
+	ownerID: 0,
+};
 
 describe("TodoView", () => {
 	let component: TodoView;
@@ -15,6 +24,7 @@ describe("TodoView", () => {
 		}).compileComponents();
 
 		fixture = TestBed.createComponent(TodoView);
+		fixture.componentRef.setInput("todo", dummyTodo);
 		component = fixture.componentInstance;
 		await fixture.whenStable();
 	});
