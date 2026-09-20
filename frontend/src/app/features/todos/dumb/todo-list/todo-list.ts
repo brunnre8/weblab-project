aafb25add345d7ac6bae9c117fb8c68215fc5fa4
Reto Brunner <reto@slightlybroken.com>
2026-09-20 21:24:05 +0200

    todo grid: add "add todo" button

--- frontend/src/app/features/todos/dumb/todo-list/todo-list.ts
+++ frontend/src/app/features/todos/dumb/todo-list/todo-list.ts
@@ -1,10 +1,13 @@
 import { Component, input } from "@angular/core";
 import { Todo } from "../../../../models/todo";
 import { TodoView } from "../todo-view/todo-view";
+import { MatButtonModule } from "@angular/material/button";
+import { MatIconModule } from "@angular/material/icon";
+import { RouterLink } from "@angular/router";
 
 @Component({
 	selector: "app-todo-list",
-	imports: [TodoView],
+	imports: [TodoView, MatButtonModule, MatIconModule, RouterLink],
 	templateUrl: "./todo-list.html",
 	styleUrl: "./todo-list.css",
 })
